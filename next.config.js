a28a4209cc4e8196b3734b54c6d1af92484258d1
AnanduPushpavadiyil <ananduomanakuttan.softwaredev@gmail.com>
2024-12-29 23:28:46 +0530

    fix: fix broken issue in build pipeline

--- next.config.js
+++ next.config.js
@@ -9,7 +9,9 @@ const nextConfig = {
 
   // Enable static export
   output: 'export', // Required for `next export`
-
+  
+  basePath: '/Alora', // Add the repository name here
+  assetPrefix: '/Alora', // Ensures static assets use the correct prefix
   // Uncomment to add domain whitelist for images
   // Ensure compatibility with static export
   images: {
